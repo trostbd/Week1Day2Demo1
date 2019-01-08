66c3d3c066c4de4237281da6e6c6631ce774f4ac
Brandon Trost <trostbd@gmail.com>
2019-01-08 12:14:09 -0500

    Committing file

---
+++ app/src/main/java/com/example/lawre/week1day2demo/SampleJavaClass.java
@@ -0,0 +1,9 @@
+package com.example.lawre.week1day2demo;
+
+public class SampleJavaClass
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Death is only the end if you assume the story is about you.");
+    }
+}
